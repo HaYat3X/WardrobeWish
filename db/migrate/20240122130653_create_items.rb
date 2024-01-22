6811f09ca4e1469c018537b631299bd7ec541fb4
Hayate12345 <hayatesato3@gmail.com>
2024-01-22 22:22:55 +0900

    Itemsテーブルの作成

---
+++ db/migrate/20240122130653_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[7.1]
+  def change
+    create_table :items do |t|
+      t.string :category, null: false
+      t.string :image, null: false
+      t.string :brand
+      t.integer :price, null: false
+      t.string :color
+      t.integer :purchas_flag
+      t.timestamps
+    end
+  end
+end
